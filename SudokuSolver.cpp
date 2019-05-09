b7f5263eb325f6a8d62f3b6c01d6ce53268bc3cd
Jayd73 <50319805+Jayd73@users.noreply.github.com>
2019-05-10 00:50:40 +0530

    Added comments.
    The algorithm used here, solves sudoku by recording all possible numbers for a cell that satisfy the condition for row, column and 3x3 block (sub-grid). It assigns one possibility to the empty cell and makes a recursive call. If there is no possibility then backtrack and change previous empty cell with another of its possibility if any. This algorithm may take a lot of time to compute some sudoku puzzles, especially those with less number of given clues like 17-25.

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -16,7 +16,7 @@ int main()
 {
    clrscr();
 //sample sudoku matrix
-//unsolved. More unsolved in the text file
+//unsolved. More unsolved in the unsolvedSudoku.txt file
  int t1[9][3][3]={{{1,0,0},{7,3,0},{4,6,0}},{{4,8,9},{0,5,0},{0,0,1}},
 		 {{0,0,6},{0,4,0},{2,9,5}},
 		 {{3,8,7},{5,0,1},{0,4,6}},{{1,2,0},{7,0,3},{0,9,5}},
@@ -46,6 +46,7 @@ int count(int arr[100], int val, int len)
     return c;
   }
 
+//this function returns possible numbers for an empty cell satisfying the 3 conditions.
 int* getNumsIn (int sudoku[9][3][3], int rowInd, int colmInd)
 {
   int* posb= new int[9];
@@ -77,16 +78,16 @@ int* getNumsIn (int sudoku[9][3][3], int rowInd, int colmInd)
 	index++;
       }
     }
-    posb[0]=index; //storing len of posb at index 0, hence elem. starts from
-		   //index 1.
+    posb[0]=index; //storing len of posb at index 0, hence elem. starts from index 1
+	
        return posb;
-
 }
 
+//solving function which will be called recursively
 int solveSudoku (int sudoku[9][3][3])
 {
-  int returnVal=0, repeat=1;
-  int incP=1;
+  int returnVal=0, repeat=1; 
+  int incP=1; 				// for moving to the next possibility.
   for (int i=0; i<9; i++)
   {
     int numInCell,ind=0;
@@ -95,56 +96,61 @@ int solveSudoku (int sudoku[9][3][3])
      {
        for (int k=0; k<3; k++)
        {
-	 numInCell=sudoku[j+(i/3)*3][i-3*(i/3)][k];
+	 numInCell=sudoku[j+(i/3)*3][i-3*(i/3)][k];	//getting one number at a time from a cell. 
 	 repeat=1;
-	 while(repeat==1)
-	 {   repeat=0;
-	 if(numInCell==0)
+	 while(repeat==1)				
+	 {   repeat=0;					//hence the code inside this loop will be repeated once.
+	 if(numInCell==0)				//checking if the cell is empty .i.e if the number in it is 0.
 	   {
-	    int* posb = getNumsIn(sudoku,i,ind);
-	    if(posb[0]==0)
+	    int* posb = getNumsIn(sudoku,i,ind);	// getting all possibilities for the empty cell in the form of an array.
+	    if(posb[0]==0)				//checking whether there are 0 possibilities by checking the length of the array.
 		{return 0;}
 	    else
 	    {
-	      sudoku[j+(i/3)*3][i-3*(i/3)][k]=posb[incP];
-	      returnVal=solveSudoku(sudoku);
+	      sudoku[j+(i/3)*3][i-3*(i/3)][k]=posb[incP];   // if there are possibilities, fill the empty cell with the next possibility.
+		    					    //based on the incP counter which starts from 1.
+		    
+	      returnVal=solveSudoku(sudoku);		    //call the function with the modified sudoku matrix and store the return value.
 	    }
 
 
-       if(returnVal !=1)
+       if(returnVal !=1)	  	//if value returned by the function called by this function is not 1, then proceed.
        {
-	   if(incP<posb[0])
+	   if(incP<posb[0])		//if there are possibilities remaining which are not entered in the empty cell.
 	      {
 		 incP++;
-		 sudoku[j+(i/3)*3][i-3*(i/3)][k]=0;
+		 sudoku[j+(i/3)*3][i-3*(i/3)][k]=0;	//empty the again from it's previous entered possibility.
 		 numInCell=0;
-		 repeat=1;
+		 repeat=1;				//repeat the while loop again for the new possibility.
 	      }
 	 else
 	    {
-		       sudoku[j+(i/3)*3][i-3*(i/3)][k]=0;
+		       sudoku[j+(i/3)*3][i-3*(i/3)][k]=0;	//if all possibilities are over empty the cell again and return 0.
 		       return 0;
 		    }
-	}//the numInCell if condition
-       }  //the if condition checking for empty sq.
-      }//while looop
-      ind++;
-     }  //k for loop
-   }   //j for loop
-  }//i for loop
-  if(returnVal!=1)
+	}	//the numInCell if condition
+       }  	//the if condition checking for empty sq.
+      }   	//while looop
+      ind++;				//incrementing the coloumn counter.
+     } 		 //k for loop
+   }   		//j for loop
+  }		//i for loop
+	
+  if(returnVal!=1)		//This condition will be checked after all cells are traversed.	
   {
-   if (check(sudoku)==0)
+   if (check(sudoku)==0)	//After traversing, checking if the sudoku is solved correctly.
    {
-	printSudoku(sudoku);
-	return 1;
+	printSudoku(sudoku);	//if the ans is correct, then print the solved sudoku and return 1. 
+	   
+	return 1;		//Returning 1 specifies that puzzle is successfully solved.
    }
   }
 
-  if (returnVal==1)
+  if (returnVal==1)		//if the returned value is 1, puzzle is solved and the current function should return 1 while going back
+	  			//to the first function that started the recursion
 	return 1;
 
-  return 0;
+  return 0;			//if sudoku is not solved .i.e returnVal=0, then this function will return 0 too.
 }
 
 //Func. to check sudoku
